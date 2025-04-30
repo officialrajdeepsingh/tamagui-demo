7f9c9ea17f76bf1df75b8878ab7d0bbd8e8470b0
Rajdeep Singh <radewala.2@gmail.com>
2025-04-30 20:51:04 +0530

    icons

---
+++ components/icons/Back.tsx
@@ -0,0 +1,17 @@
+export function Back() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m15 18-6-6 6-6" />
+    </svg>
+  );
+}
